ab31e3e052d807cf1f081fd486523b7b83731254
samraattiwari <163579927+samraattiwari@users.noreply.github.com>
2025-01-19 16:39:59 +0530

    Add files via upload

---
+++ Backend/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import { app,server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () => {
+  console.log("Server is running on port:" + PORT);
+  connectDB();
+});
